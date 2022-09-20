895bac4045cd28d785e70533c2b294e02302d5cb
IamSanchitSharma <sanchit12sharma@gmail.com>
2022-09-20 09:37:49 -0400

    "Project added: My first commit :)"

---
+++ labexercise-petclinic/src/main/java/ca/gbc/comp3095/labexercise_petclinic/LabexercisePetclinicApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.labexercise_petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabexercisePetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabexercisePetclinicApplication.class, args);
+	}
+
+}
---
+++ labexercise-petclinic/src/test/java/ca/gbc/comp3095/labexercise_petclinic/LabexercisePetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.labexercise_petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabexercisePetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
